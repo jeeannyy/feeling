82d2010a4d1b9e21bf3728dfbf6d1117681819d4
jeeannyy <jeeann.kim@gmail.com>
2025-02-16 20:23:23 +0000

    fix url

--- server/server.js
+++ server/server.js
@@ -6,13 +6,7 @@ const cors = require('cors');
 const MoodSong = require('./models/MoodSong');
 
 const app = express();
-app.use(
-	cors({
-		origin: '*',
-		methods: ['GET', 'POST'],
-		allowedHeaders: ['Content-Type'],
-	}),
-);
+app.use(cors());
 app.use(express.json());
 
 const MONGO_URI = process.env.MONGO_URI;
